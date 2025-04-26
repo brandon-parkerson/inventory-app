caea81569715d825d3c07f40f650d9dfb17af567
brandon-parkerson <brandon-m-parkerson@gmail.com>
2025-04-26 14:04:02 +0300

    server running

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const usersRouter = require("./routes/usersRouter");
+
+
+// set view engine
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true}));
+
+app.use("/", usersRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Express app listening on port: ${PORT}`));
