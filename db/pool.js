f0d3757a26bceedd0abfe280505c04c1a5a7b7a9
brandon-parkerson <brandon-m-parkerson@gmail.com>
2025-04-27 14:54:34 +0300

    connection string

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,5 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-    host: process.env.HOST, // or wherever the db is hosted
-    user: process.env.USER,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.DATABASE_PORT // The default port
+    connectionString: "postgresql://postgres:VZmeunFWKTziJdJMsaexvCqfxpNjaDFZ@yamabiko.proxy.rlwy.net:30980/railway"
   });
