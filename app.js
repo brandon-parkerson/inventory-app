6d5d4d8e9883b9829a290bb99e5e0090163a5feb
brandon-parkerson <brandon-m-parkerson@gmail.com>
2025-04-27 14:26:37 +0300

    little bit of styling

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true}));
 
 app.use("/", usersRouter);
+app.use(express.static('public'));
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port: ${PORT}`));
